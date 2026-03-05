a816e4b27ca9c04153ec5b050c3950183a753fb5
hadi542 <77535129+hadi542@users.noreply.github.com>
2026-03-05 20:19:28 +0530

    Create main landing page component file

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Services from '../components/Services';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
